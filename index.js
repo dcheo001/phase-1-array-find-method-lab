f185cb54d85c04282599668324a0606ab72e02c8
Daniel Cheong <dcheo001@gmail.com>
2021-11-04 13:48:28 -0400

    commit tree d895330a187bbc9827b3293a9184da1be4f4b073

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+]
+// function superbowlWin(record) {
+//     if (record.result === "W") {
+//         return record.year;
+//     }
+// }
+
+// let superbowlWin = record.find(record => record.year === "W")
+// console.log(superbowlWin)
+
+function superbowlWin(record) {
+    const win = record.find(record => record.result === "W"); //
+    if (win){
+        return win.year
+    } else {
+        return 
+    }  
+}   superbowlWin();
